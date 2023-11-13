7287d0aa0c167fd782e91600bdf75db360d7cf1d
unknown <vedican.v44@gmail.com>
2023-11-13 12:28:00 +0530

    first workflow

--- frontendwithredux/my_app/src/components/Product/Product.jsx
+++ frontendwithredux/my_app/src/components/Product/Product.jsx
@@ -1,43 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProductData, DeleteProduct } from '../Redux/actions';
 import { Link } from 'react-router-dom';
 
 function Product() {
-
   const [search, setSearch] = useState('');
-  const [sorting , setsorting] = useState("");
-  const [filter, setfilter]= useState("");
+  const [sorting, setsorting] = useState('');
+  const [filter, setfilter] = useState('');
   const [page, setpage] = useState(1);
-
- console.log(page);
-
-  const productdata = useSelector((state) => state.product.productdata);
-  const tatalpage = useSelector((state) => state.product.totalcount);
-
-  console.log(tatalpage);
-
+  const [count , setCount] = useState(1);
   const dispatch = useDispatch();
 
-  const disablepage = Math.ceil(tatalpage/4)
-  console.log(disablepage)
+
+  console.log(sorting);
+
+  const productdata = useSelector((state) => state.product.productdata);
+  const totalPage = useSelector((state) => state.product.totalcount);
+
+  const disablepage = useMemo(() => Math.ceil(totalPage / 4), [totalPage]);
+
+  const memoizedFetchProductData = useMemo(() => {
+    return () => dispatch(fetchProductData(search, sorting, filter, page));
+  }, [dispatch, search, sorting, filter, page]);
 
   useEffect(() => {
-    dispatch(fetchProductData(search,sorting,filter,page));
+    memoizedFetchProductData();
+  }, [memoizedFetchProductData]);
 
-  }, [search,sorting,filter,page]);
- 
-
+  
   const handleDelete = (id) => {
-    dispatch(DeleteProduct(id))
-      dispatch(fetchProductData());
+    dispatch(DeleteProduct(id));
+    memoizedFetchProductData();
   };
 
+  const increase = ()=>{
+    setCount(count +1);
+  }
+  const dicrease = ()=>{
+    setCount(count-1);
+  }
   
   return (
     <div>
       Productdiv
-
+       <div>
+        <button onClick={increase}>inc</button>
+        <p>{count}</p>
+        <button onClick={dicrease}>dic</button>
+       </div>
       <div>
         <label htmlFor="">Serch BY NAME</label>
         <input type="text" onChange={(e)=> setSearch(e.target.value)} />
